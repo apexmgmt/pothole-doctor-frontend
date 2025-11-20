924a099d341aeaf069e4a59d0f68a6ea5d59db6d
Arman <mdalarmansorker@gmail.com>
2025-11-20 12:41:37 +0600

    company api updated to organization

--- constants/api/company_api.ts
+++ constants/api/company_api.ts
@@ -1,5 +1,5 @@
 /**Companies API: GET, POST, PUT, GET, DELETE */
-export const COMPANIES: string = '/v1/companies/'
+export const COMPANIES: string = '/v1/organizations/'
 
 /** Company Status Update API: POST */
-export const COMPANY_STATUS_CHANGE: string = '/v1/company-status-change'
+export const COMPANY_STATUS_CHANGE: string = '/v1/organization-status-change'
