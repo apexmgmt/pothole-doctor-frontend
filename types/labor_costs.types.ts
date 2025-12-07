9c84aeddb01460b26548be09e1b50969eb8873b9
Arman <mdalarmansorker@gmail.com>
2025-12-07 17:32:08 +0600

    labor cost types add

--- types/labor_costs.types.ts
+++ types/labor_costs.types.ts
@@ -1,3 +1,29 @@
+import { ServiceType } from "./service_types.types"
+import { Unit } from "./units.types"
+
 export interface LaborCost {
-    id: string;
-    }
+  id: string
+  name: string
+  description: string
+  cost: number
+  price: number
+  margin: number
+  service_type_id: string
+  service_type?: ServiceType
+  unit_id: string
+  unit?: Unit
+  created_at: string
+  updated_at: string
+}
+
+export interface LaborCostPayload {
+    name: string
+    description: string
+    cost: number
+    price: number
+    margin: number
+    service_type_id: string
+    unit_id: string 
+}
+
+
