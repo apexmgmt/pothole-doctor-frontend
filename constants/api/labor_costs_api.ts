a1f6b2edb1e375983451ce7b37981c71730d778e
Arman <mdalarmansorker@gmail.com>
2025-12-07 17:09:08 +0600

    labor cost api declare

--- constants/api/labor_costs_api.ts
+++ constants/api/labor_costs_api.ts
@@ -1,1 +1,14 @@
-export const LABOR_COSTS: string = '';
+/**
+ * * API endpoint for labor costs.
+ * 
+ * @method: GET - Retrieve labor costs data table.
+ * 
+ * @method: POST - Create a new labor cost entry.
+ * 
+ * @method: PUT - Update an existing labor cost entry. /id
+ * 
+ * @method: GET - Retrieve a specific labor cost entry by ID. /id
+ * 
+ * @method: DELETE - Remove a specific labor cost entry by ID. /id
+ */
+export const LABOR_COSTS: string = '/v1/labor-costs/'
