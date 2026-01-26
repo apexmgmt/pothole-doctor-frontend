054cc975e45c7e677d81d2418cd4426db677967c
Arman <mdalarmansorker@gmail.com>
2026-01-26 15:50:48 +0600

    fix: toaster position update

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -11,6 +11,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps['theme']}
       className='toaster group'
+      position='top-right'
+      visibleToasts={10}
       icons={{
         success: <CircleCheckIcon className='size-4' />,
         info: <InfoIcon className='size-4' />,
