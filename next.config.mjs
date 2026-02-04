2e86ea754a65a510a322b00d3c7d2f5b48fbbe40
Arman <mdalarmansorker@gmail.com>
2026-02-04 12:14:55 +0600

    feat (console): remove console on production

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'potholedoctors.dev',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '8000',
-                pathname: '/**',
-            },
-        ],
-    },
-    allowedDevOrigins: [
-        'localhost',
-        '127.0.0.1',
-        'potholedoctor.test', // Add your dev domain(s) here
-        '*.potholedoctor.test', // If you use subdomains
-    ],
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'potholedoctors.dev',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/**'
+      }
+    ]
+  },
+  allowedDevOrigins: [
+    'localhost',
+    '127.0.0.1',
+    'potholedoctor.test', // Add your dev domain(s) here
+    '*.potholedoctor.test' // If you use subdomains
+  ],
+  compiler: {
+    // Remove all console.* calls in production
+    removeConsole: process.env.NODE_ENV === 'production'
+  }
+}
 
-export default nextConfig;
+export default nextConfig
