7ce98ded15289231682a2866be7133ab2a00f047
Arman <mdalarmansorker@gmail.com>
2026-03-05 15:33:12 +0600

    feat: enhance InvoiceService with API methods for fetching and viewing invoices; update Invoice interface with additional fields

--- services/api/invoices.service.ts
+++ services/api/invoices.service.ts
@@ -1,7 +1,66 @@
+import { API_URL, INVOICES, VIEW_INVOICE } from '@/constants/api'
+import apiInterceptor from './api.interceptor'
+
 export default class InvoiceService {
-    static index = async () => {};
-    static store = async () => {};
-    static show = async () => {};
-    static update = async () => {};
-    static destroy = async () => {};
+  /**
+   * Fetches a list of invoices based on the provided filter options.
+   *
+   * @param filterOptions An object containing key-value pairs for filtering the invoices.
+   * @returns A promise that resolves to the list of invoices matching the filter criteria.
+   * @throws An error if the API request fails or returns a non-OK response.
+   */
+  static index = async (filterOptions: object) => {
+    try {
+      const queryParams = new URLSearchParams(filterOptions as Record<string, string>).toString()
+
+      const response = await apiInterceptor(API_URL + INVOICES + (queryParams ? `?${queryParams}` : ''), {
+        requiresAuth: true,
+        method: 'GET',
+        next: { revalidate: 60, tags: ['invoices'] } // Cache for 60 seconds
+      })
+
+      if (!response.ok) {
+        const errorData = await response.json()
+
+        throw new Error(errorData.message || 'Failed to fetch invoices')
+      }
+
+      return await response.json()
+    } catch (error) {
+      throw error
     }
+  }
+
+  static store = async () => {}
+  static show = async () => {}
+  static update = async () => {}
+  static destroy = async () => {}
+  static restore = async (invoice_id: string) => {}
+
+  /**
+   * Views an invoice based on the provided invoice hash ID and client hash ID.
+   *
+   * @param invoiceHashId The hash ID of the invoice to be viewed.
+   * @param clientHashId The hash ID of the client associated with the invoice.
+   * @returns A promise that resolves to the invoice data if the request is successful.
+   * @throws An error if the API request fails or returns a non-OK response.
+   */
+  static viewInvoice = async (invoiceHashId: string, clientHashId: string) => {
+    try {
+      const response = await apiInterceptor(API_URL + VIEW_INVOICE(invoiceHashId, clientHashId), {
+        requiresAuth: false,
+        method: 'GET'
+      })
+
+      if (!response.ok) {
+        const errorData = await response.json()
+
+        throw new Error(errorData.message || 'Failed to view invoice')
+      }
+
+      return await response.json()
+    } catch (error) {
+      throw error
+    }
+  }
+}
--- types/invoices.d.ts
+++ types/invoices.d.ts
@@ -1,3 +1,48 @@
+import { BusinessLocation } from "./business_location"
+import { Client } from "./clients/clients"
+import { EstimateType } from "./estimate_types"
+import { Estimate, TakeoffData } from "./estimates/estimates"
+import { Proposal, ProposalService } from "./estimates/proposals"
+
 export interface Invoice {
-    id: string;
-    }
+  id: string
+  invoice_number: number
+  estimate_id: string | null
+  estimate?: Estimate
+  proposal_id: string | null
+  proposal?: Proposal
+  invoice_type_id: string 
+  invoice_type?: EstimateType
+  client_id: string
+  client?: Client
+  assign_id: string 
+  created_by: string 
+  payment_term_id: string
+  service_type_id: string
+  title: string
+  interaction: string | 'cash_and_pickup' | 'cash_and_delivery' | '' | null
+  pickup_date: string | null
+  pickup_location_id: string | null
+  pickup_location?: BusinessLocation | null
+  pickup_notes: string | null
+  delivery_datetime: string | null
+  delivery_location: string | null
+  delivery_notes: string | null
+  tax_rate: number
+  status: string
+  issue_date: string | null
+  due_date: string | null
+  discount: number
+  subtotal: number
+  sale_tax: number
+  total: number
+  message: string | null
+  reason: string | null
+  take_off_data: TakeoffData | null
+  deleted_at: string | null
+  created_at: string
+  updated_at: string
+  services?: ProposalService[]
+}
+
+
