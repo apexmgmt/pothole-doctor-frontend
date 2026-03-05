1a2a44ca01aa06be47812d8b723c319832bf9a8c
Arman <mdalarmansorker@gmail.com>
2026-03-05 15:07:51 +0600

    feat: add comprehensive API endpoints for invoices and related services

--- constants/api/invoices_api.ts
+++ constants/api/invoices_api.ts
@@ -1,1 +1,58 @@
-export const INVOICES: string = '';
+/**
+ * This contains all the API endpoints related to invoices.
+ *
+ * @method GET - Data table operation method: GET
+ * @method POST - Data create method: POST (Only create the initial part of the invoice not the services)
+ * @method PUT - Data update method: PUT /{id} (Only update the initial part of the invoice not the services)
+ * @method GET - Data retrieve method: GET /{id}
+ * @method DELETE - Data delete method: DELETE /{id}
+ */
+export const INVOICES: string = '/v1/tenant/invoices/'
+
+/**
+ * This contains all the API endpoints related to invoice services.
+ * @param invoiceId string (uuid)
+ * @method POST - To add a service to an invoice
+ * @method PUT - To update a service in an invoice
+ * @returns 
+ */
+export const INVOICES_SERVICES = (invoiceId: string): string => {
+  return `/v1/tenant/invoices/${invoiceId}/services/`
+}
+
+/**
+ * This contains the API endpoint to restore a deleted invoice.
+ * @param invoiceId string (uuid)
+ * @method PUT - To restore a deleted invoice
+ * @returns 
+ */
+export const INVOICES_RESTORE = (invoiceId: string): string => {
+  return `/v1/tenant/invoices/${invoiceId}/restore/`
+}
+
+/**
+ * This contains the API endpoint to get all invoice documents.
+ * @method GET - To get data table of invoice documents
+ * @method POST - To upload a new invoice document
+ * @method GET - To get a specific invoice document (document_id should be passed as query param)
+ * @method PUT - To update a specific invoice document (document_id should be passed as query param)
+ * @method DELETE - To delete a specific invoice document (document_id should be passed as query param)
+ * @returns 
+ */
+export const INVOICE_DOCUMENTS: string = '/v1/tenant/invoice-documents/'
+
+/**
+ * This contains the API endpoint to view an invoice.
+ * @param inid string (invoice hash id)
+ * @param icid string (client hash id)
+ */
+export const VIEW_INVOICE = (inid: string, icid: string): string => {
+    return `/v1/tenant/view-invoice/?inid=${inid}&icid=${icid}`
+}
+
+/**
+ * This contains the API endpoint to approve an invoice.
+ * @method POST - To approve an invoice
+ * @param invoice_id string (uuid)
+ */
+export const APPROVE_INVOICE: string = '/v1/tenant/approve-invoice/'
