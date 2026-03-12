91da360be3b1acba04bc17984bfe4d0551d08e3c
Arman <mdalarmansorker@gmail.com>
2026-03-12 16:32:34 +0600

    feat: add down payment fields to Invoice interface

--- types/invoices.d.ts
+++ types/invoices.d.ts
@@ -52,6 +52,9 @@ export interface Invoice {
   payment_method_data?: Record<string, string> | null
   is_agreed_terms?: boolean | number | null
   is_signed?: boolean
+  is_down_payment_materials: boolean
+  down_payment_amount: number
+  down_payment_percentage: number
 }
 
 export interface InvoicePayload {
