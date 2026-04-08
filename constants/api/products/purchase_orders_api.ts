79244ce0d232752e295a0fd03196b37b845cad4a
Md Al Arman Sorker <mdalarmansorker@gmail.com>
2026-04-08 11:42:07 +0600

    feat: define Purchase Orders API endpoints and reintroduce PurchaseOrderService class

--- constants/api/products/purchase_orders_api.ts
+++ constants/api/products/purchase_orders_api.ts
@@ -1,1 +1,10 @@
-export const PURCHASE_ORDERS: string = '';
+/**
+ * Purchase Orders API Endpoints
+ *
+ * Data table operation method: GET
+ * Data create method: POST
+ * Data update method: PUT /{id}
+ * Data retrieve method: GET /{id}
+ * Data delete method: DELETE /{id}
+ */
+export const PURCHASE_ORDERS: string = '/v1/tenant/purchase_orders/';
