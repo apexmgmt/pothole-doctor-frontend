fbab3725071f86aaaf9cb6f2eb63fdd30555d835
Md Al Arman Sorker <mdalarmansorker@gmail.com>
2026-04-11 16:51:25 +0600

    fix: correct property name for customer price in purchase order payload

--- views/erp/products/purchase-orders/CreateOrEditPurchaseOrderModal.tsx
+++ views/erp/products/purchase-orders/CreateOrEditPurchaseOrderModal.tsx
@@ -233,7 +233,7 @@ const CreateOrEditPurchaseOrderModal = ({
       quantity: p.quantity,
       work_order_cost: p.work_order_cost,
       margin: p.margin,
-      customer_price: p.p
+      customer_price: p.customer_price
     }))
 
     const payload: PurchaseOrderPayload = {
