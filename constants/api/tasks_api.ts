32ebab9d34b07a827cadde45962c930035ab4724
Md Al Arman Sorker <mdalarmansorker@gmail.com>
2026-05-04 16:59:25 +0600

    feat: add TaskDocumentService for managing task documents API operations

--- constants/api/tasks_api.ts
+++ constants/api/tasks_api.ts
@@ -43,3 +43,14 @@ export const TASKS_STATUS_TENANT = (taskId: string): string => `/v1/tenant/tasks
  * @returns 
  */
 export const TASKS_COMMENTS = (taskId: string): string => `/v1/tenant/tasks/${taskId}/comments/`
+
+/**
+ * This contains the API endpoint to get all task documents.
+ * @method GET - To get data table of task documents
+ * @method POST - To upload a new task document
+ * @method GET - To get a specific task document (document_id should be passed as query param)
+ * @method PUT - To update a specific task document (document_id should be passed as query param)
+ * @method DELETE - To delete a specific task document (document_id should be passed as query param)
+ * @returns
+ */
+export const TASK_DOCUMENTS: string = '/v1/tenant/task-documents/'
